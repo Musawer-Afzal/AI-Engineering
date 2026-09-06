e399ffa03efcb17873318d9ea77f453e11d45477
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-06 15:40:59 +0500

    Multiprocessing works correctly in .py files

---
+++ Python/Multithreading and Miltiprocessing/multiprocess.py
@@ -0,0 +1,26 @@
+import time
+import multiprocessing as mp
+
+def calc_square(num):
+    for n in num:
+        print(f"Square: {n ** 2}")
+
+def calc_cube(num):
+    for n in num:
+        print(f"Cube: {n ** 3}")
+
+
+if __name__ == "__main__":
+    arr = [4, 7, 5, 7]
+
+    p1 = mp.Process(target = calc_square, args = (arr,))
+    p2 = mp.Process(target = calc_cube, args = (arr,))
+
+    t = time.time()
+    p1.start()
+    p2.start()
+
+    p1.join()
+    p2.join()
+
+    print(f"Done in: {time.time() - t} seconds")
