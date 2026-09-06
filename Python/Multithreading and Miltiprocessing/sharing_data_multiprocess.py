c7861b27f4b596895d24ec68f6d5bf5222e4f3ce
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-06 16:05:18 +0500

    Chile process is updating the value, Parent can still access

--- Python/Multithreading and Miltiprocessing/sharing_data_multiprocess.py
+++ Python/Multithreading and Miltiprocessing/sharing_data_multiprocess.py
@@ -1,7 +1,8 @@
 import time
 import multiprocessing as mp
 
-def calc_square(num, result):
+def calc_square(num, result, v):
+    v.value = 86.53 # We are actually assigning the value here in this process
     for idx, n in enumerate(num):
         result[idx] = n*n
 
@@ -9,12 +10,14 @@ def calc_square(num, result):
 if __name__ == "__main__":
     arr = [4, 7, 5, 7]
     result = mp.Array('i', 4)
+    v = mp.Value('d', 0.0) # We are intentionally leaving the value as 0
 
-    p1 = mp.Process(target = calc_square, args = (arr, result))
+    p1 = mp.Process(target = calc_square, args = (arr, result, v))
     t = time.time()
     p1.start()
 
     p1.join()
 
-    print(f"Result: {result[:]}")
+    # The Value will be available at the main/Parent Process
+    print(f"Result: {result[:]}\nValue: {v.value}")
     print(f"Done in: {time.time() - t} seconds")
