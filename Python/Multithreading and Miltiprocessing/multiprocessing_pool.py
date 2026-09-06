708f09996db6a22dd7e9e6bccc0e9b74673a1e37
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-06 19:45:07 +0500

    Dividing tasks among cores using Pool

---
+++ Python/Multithreading and Miltiprocessing/multiprocessing_pool.py
@@ -0,0 +1,40 @@
+# Using normal function we are only taking advantage of one core
+# def f(n):
+#     return n*n
+
+# if __name__ == "__main__":
+#     array = [1, 2, 3, 4, 5]
+#     result = []
+#     for n in array:
+#         result.append(f(n))
+
+#     print(result)
+
+
+# Now lets do the above process using multiprocessing
+from multiprocessing import Pool
+import time
+
+def f(n):
+    sum = 0
+    for x in range(10000):
+        sum += x*x
+
+    return sum
+
+
+if __name__ == "__main__":
+    t1 = time.time()
+    p = Pool()
+    result = p.map(f, range(10000))
+    p.close()
+    p.join()
+
+    print("Pool Time: ", time.time() - t1, " seconds")
+
+    t2 = time.time()
+    result = []
+    for x in range(10000):
+        result.append(f(x))
+
+    print("Normal Time: ", time.time() - t2, " seconds")
