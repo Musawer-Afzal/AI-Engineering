0e07c3c94d2abc02a2c4494414d7fa7d03acd1a3
Musawer Afzal <musawerafzal23@gmail.com>
2026-09-06 19:47:26 +0500

    Controlling the number of cores used using processes

--- Python/Multithreading and Miltiprocessing/multiprocessing_pool.py
+++ Python/Multithreading and Miltiprocessing/multiprocessing_pool.py
@@ -15,26 +15,37 @@
 from multiprocessing import Pool
 import time
 
-def f(n):
-    sum = 0
-    for x in range(10000):
-        sum += x*x
+# def f(n):
+#     sum = 0
+#     for x in range(10000):
+#         sum += x*x
 
-    return sum
+#     return sum
+
+
+# if __name__ == "__main__":
+#     t1 = time.time()
+#     p = Pool()
+#     result = p.map(f, range(10000))
+#     p.close()
+#     p.join()
+
+#     print("Pool Time: ", time.time() - t1, " seconds")
+
+#     t2 = time.time()
+#     result = []
+#     for x in range(10000):
+#         result.append(f(x))
+
+#     print("Normal Time: ", time.time() - t2, " seconds")
+
+
+def f(n):
+    return n*n
 
 
 if __name__ == "__main__":
-    t1 = time.time()
-    p = Pool()
-    result = p.map(f, range(10000))
-    p.close()
-    p.join()
-
-    print("Pool Time: ", time.time() - t1, " seconds")
-
-    t2 = time.time()
-    result = []
-    for x in range(10000):
-        result.append(f(x))
-
-    print("Normal Time: ", time.time() - t2, " seconds")
+    p = Pool(processes = 3)
+    result = p.map(f, [1, 2, 3, 4, 5])
+    for n in result:
+        print(n)
